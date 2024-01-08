4f8096a568b020b333df4076372d9cb591fabbbd
pamelakelly0023 <pamela.kelly95@gmail.com>
2024-01-08 17:50:12 -0300

    Criacao do projeto

---
+++ Program.cs
@@ -0,0 +1,22 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapGet(
+    "/all", () => new Todo[]{ new Todo(1, "teste"), new(2, "teste-2")});
+
+app.Run();
+record Todo(int Id, string Title);
